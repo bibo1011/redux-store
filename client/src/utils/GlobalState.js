6a803021f81a666f5f18ac3c67885b35c2515d12
Blue Abad <bibo1011@gmail.com>
2021-01-03 22:52:03 -0800

    implement global state in replace of useQuery

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,24 @@
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from './reducers';
+
+// data maintained by global state
+// eventually be able to add offline capabilities for our app with IndexedDB
+// will make adding those capabilities much easier than having to do it to each useQuery() Hook's response
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        categories: [],
+        currentCategory: '',
+    });
+    // use this to confirm it works!
+    console.log(state);
+    return <Provider value={[state, dispatch]} {...props} />;
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+export { StoreProvider, useStoreContext };
