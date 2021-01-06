40c62734d0a4062a7d90e030b2d92286adca1ca7
Blue Abad <bibo1011@gmail.com>
2021-01-05 23:36:01 -0800

    add checkout function

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -35,6 +35,7 @@ const CartItem = ({ item }) => {
             idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
         }
     };
+   
 
     return (
         <div className="flex-row">
